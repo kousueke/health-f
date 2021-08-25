5984a71320ab82f921f8414ff63ad88060f95ba1
kousueke <k.koko.koko.kousuke@gmail.com>
2021-08-25 23:05:06 +0900

    middlewareテスト

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,10 +2,10 @@ import firebase from '../plugins/firebase'
 
 export default function ({ redirect }) {
   firebase.auth().onAuthStateChanged((user) => {
-    // console.log("成功");
+    console.log("成功");
     if (!user) {
       redirect('/login')
-      // console.log("失敗（処理は成功）");
+      console.log("失敗（処理は成功）");
     }
   })
 }
